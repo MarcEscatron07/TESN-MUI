afbd07ba35911afca5a84a06264ddcc838a6a348
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-07-15 16:03:19 +0800

    applied custom CSS for Fullcalendar

--- components/EventCalendar/index.js
+++ components/EventCalendar/index.js
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useTheme } from "@mui/material/styles";
-import { Scheduler } from "@aldabil/react-scheduler";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import moment from 'moment-timezone';
 import Holidays from 'date-holidays';
 
@@ -12,24 +13,9 @@ export default function EventCalendar() {
     const theme = useTheme();
     const hd = new Holidays('PH');
 
-    const schedulerRef = useRef(null);
-
     const [locHDList, setLocHDList] = useState([]);
     const [regHDList, setRegHDList] = useState([]);
     const [customEventsList, setCustomEventsList] = useState([]);
-
-    const eventFields = [
-        {
-            name: 'link',
-            type: 'input',
-            config: { label: "Link" }
-        },
-        {
-            name: 'description',
-            type: 'input',
-            config: { label: "Description" }
-        },
-    ];
 
     useEffect(() => {
         fetchHolidays();
@@ -53,21 +39,14 @@ export default function EventCalendar() {
                 setLocHDList(
                     res ? res.map((item, idx) => {
                         return {
-                            event_id: idx + 1,
+                            id: idx + 1,
                             title: item?.name,
                             link: item?.link,
                             description: item?.description,
                             start: moment(`${item?.date}, ${moment().year()}`).toDate(),
                             end: moment(`${item?.date}, ${moment().year()}`).toDate(),
-                            disabled: false,
-                            color: theme.palette.accent1.main,
-                            textColor: theme.palette.accent1.contrastText,
-                            editable: true,
-                            deletable: false, // should be 'false' for holidays
-                            draggable: false, // should be 'false' for holidays
-                            allDay: true, // should be 'true' for holidays
-                            // agendaAvatar: <></>,
-                            // sx: {}
+                            classNames: ['fc-custom-event', 'fc-local-holiday'],
+                            extendedProps: []
                         }
                     }) : []
                 )
@@ -91,21 +70,14 @@ export default function EventCalendar() {
                     res ? res.map((item, idx) => {
                         if(['public', 'optional'].includes(item?.type)) {
                             return {
-                                event_id: idx + 1,
+                                id: idx + 1,
                                 title: item?.name,
                                 link: item?.link,
                                 description: item?.description,
                                 start: moment(item?.date).toDate(),
                                 end: moment(item?.date).toDate(),
-                                disabled: false,
-                                color: theme.palette.accent2.main,
-                                textColor: theme.palette.accent2.contrastText,
-                                editable: true,
-                                deletable: false, // should be 'false' for holidays
-                                draggable: false, // should be 'false' for holidays
-                                allDay: true, // should be 'true' for holidays
-                                // agendaAvatar: <></>,
-                                // sx: {}
+                                classNames: ['fc-custom-event', 'fc-regular-holiday'],
+                                extendedProps: []
                             }
                         } else {
                             return null;
@@ -120,55 +92,15 @@ export default function EventCalendar() {
         );
     }
 
-    const onConfirmEvent = (event, action) => {
-        console.log('onConfirmEvent > event', event)
-        console.log('onConfirmEvent > action', action)
-
-        switch (action) {
-            case 'create':
-                setCustomEventsList((prevState) => [
-                    ...prevState, 
-                    {
-                        event_id: event?.event_id,
-                        title: event?.title,
-                        link: event?.link,
-                        description: event?.description,
-                        start: event?.start,
-                        end: event?.end,
-                        disabled: false,
-                        color: theme.palette.primary.main,
-                        textColor: theme.palette.primary.contrastText,
-                        editable: true,
-                        deletable: true, // should be 'false' for holidays
-                        draggable: true, // should be 'false' for holidays
-                        allDay: false, // should be 'true' for holidays
-                        // agendaAvatar: <></>,
-                        // sx: {}
-                    }
-                ]);
-                break;
-            default:
-                break;
-        }
-    }
-
     return (
-        <Scheduler
-            ref={schedulerRef}
-            height={700}
-            view="month"
-            agenda={false}
-            alwaysShowAgendaDays={false}
-            selectedDate={moment()}
-            navigation={true}
-            disableViewNavigator={false}
-            fields={eventFields}
+        <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
             events={[
                 ...locHDList, 
                 ...regHDList, 
                 ...customEventsList
             ]}
-            onConfirm={onConfirmEvent}
         />
     )
 }
