3b379d02d724818aa59a89590eae23b1aae1cac2
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-07-17 11:52:09 +0800

    setup values based on date clicked from Calendar

--- components/EventCalendar/index.js
+++ components/EventCalendar/index.js
@@ -3,13 +3,30 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "@mui/material/styles";
 import FullCalendar from '@fullcalendar/react';
+import momentPlugin  from '@fullcalendar/moment';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin  from '@fullcalendar/timegrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from "@fullcalendar/interaction";
 import bootstrapPlugin from "@fullcalendar/bootstrap";
 import listPlugin from "@fullcalendar/list";
 import moment from 'moment-timezone';
 import Holidays from 'date-holidays';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import InputAdornment from "@mui/material/InputAdornment";
+
+import { DateTimeField } from '@mui/x-date-pickers/DateTimeField';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 import { getLocalHolidays } from "@/lib/api";
 
@@ -21,10 +38,18 @@ export default function EventCalendar() {
     const [regHDList, setRegHDList] = useState([]);
     const [customEventsList, setCustomEventsList] = useState([]);
 
+    const [modalOpen, setIsModalOpen] = useState(false);
+    const [eventTitle, setEventTitle] = useState('');
+    const [eventStart, setEventStart] = useState(moment());
+    const [eventEnd, setEventEnd] = useState(moment());
+    const [eventDesc, setEventDesc] = useState('');
+    const [eventLink, setEventLink] = useState('');
+
     useEffect(() => {
         fetchHolidays();
     }, [])
 
+    /** FULLCALENDAR useEffect **/
     useEffect(() => {
         console.log('EventCalendar > locHDList', locHDList)
     }, [locHDList])
@@ -36,6 +61,31 @@ export default function EventCalendar() {
     useEffect(() => {
         console.log('EventCalendar > customEventsList', customEventsList)
     }, [customEventsList])
+    /** FULLCALENDAR useEffect **/
+
+    /** MODAL useEffect **/
+    useEffect(() => {
+        console.log('EventCalendar > eventTitle', eventTitle)
+    }, [eventTitle])
+
+    useEffect(() => {
+        console.log('EventCalendar > eventStart', eventStart)
+        console.log('EventCalendar > typeof eventStart', typeof eventStart)
+    }, [eventStart])
+
+    useEffect(() => {
+        console.log('EventCalendar > eventEnd', eventEnd)
+        console.log('EventCalendar > typeof eventEnd', typeof eventEnd)
+    }, [eventEnd])
+
+    useEffect(() => {
+        console.log('EventCalendar > eventDesc', eventDesc)
+    }, [eventDesc])
+
+    useEffect(() => {
+        console.log('EventCalendar > eventLink', eventLink)
+    }, [eventLink])
+    /** MODAL useEffect **/
 
     async function fetchHolidays() {
         await getLocalHolidays().then(
@@ -72,7 +122,7 @@ export default function EventCalendar() {
             (res) => {
                 setRegHDList(
                     res ? res.map((item, idx) => {
-                        if(['public', 'optional'].includes(item?.type)) {
+                        if (['public', 'optional'].includes(item?.type)) {
                             return {
                                 id: idx + 1,
                                 title: item?.name,
@@ -96,6 +146,7 @@ export default function EventCalendar() {
         );
     }
 
+    /** FULLCALENDAR FUNCTIONS **/
     const onEventClick = (event) => {
         console.log('onEventClick > event', event)
         alert('Event clicked!')
@@ -103,32 +154,147 @@ export default function EventCalendar() {
 
     const onDateClick = (event) => {
         console.log('onDateClick > event', event)
-        alert('Date clicked!')
+
+        setEventStart(moment(event.date));
+        setEventEnd(moment(event.date));
+
+        setIsModalOpen(true);
+    }
+    /** FULLCALENDAR FUNCTIONS **/
+
+    /** MODAL FUNCTIONS **/
+    const onModalToggleClick = (value) => {
+        setIsModalOpen(value);
     }
 
+    const onModalFormSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log('onModalFormSubmit > formJson', formJson)
+
+        const eventObj = {
+            ...formJson,
+            start: moment(formJson?.start).toDate(),
+            end: moment(formJson?.end).toDate(),
+            classNames: ['fc-custom-event', 'fc-added-event'],
+            extendedProps: []
+        }
+
+        if(customEventsList.length == 0) {
+            setCustomEventsList([{...eventObj, id: 1}]);
+        } else {
+            setCustomEventsList((prevState) => [...prevState, { ...eventObj, id: customEventsList.length + 1 }]);
+        }
+
+        onModalToggleClick(false);
+    }
+    /** MODAL FUNCTIONS **/
+
     return (
-        <FullCalendar
-            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin, bootstrapPlugin ]}
-            initialView="dayGridMonth"
-            themeSystem="bootstrap5"
-            editable={true}
-            selectable={true}
-            dayMaxEvents={true} // allow "more" link when too many events
-            displayEventTime={true}
-            displayEventEnd={true}
-            eventTimeFormat={{ // like '14:30:00'
+        <>
+            <FullCalendar
+                plugins={[momentPlugin, dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin, bootstrapPlugin]}
+                initialView="dayGridMonth"
+                themeSystem="bootstrap5"
+                editable={true}
+                selectable={true}
+                dayMaxEvents={true} // allow "more" link when too many events
+                displayEventTime={true}
+                displayEventEnd={true}
+                eventTimeFormat={{ // like '14:30:00'
                     hour: '2-digit',
                     minute: '2-digit',
                     hour12: false,
                     meridiem: false
-            }}
-            events={[
-                ...locHDList, 
-                ...regHDList, 
-                ...customEventsList
-            ]}
-            eventClick={onEventClick}
-            dateClick={onDateClick}
-        />
+                }}
+                events={[
+                    ...locHDList,
+                    ...regHDList,
+                    ...customEventsList
+                ]}
+                eventClick={onEventClick}
+                dateClick={onDateClick}
+            />
+
+            <Dialog
+                open={modalOpen}
+                onClose={() => onModalToggleClick(false)}
+                PaperProps={{ component: 'form', onSubmit: (event) => onModalFormSubmit(event) }}
+            >
+                <DialogTitle>Add Event</DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={2} sx={{p: 1}}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                margin="none"
+                                label="Title"
+                                id="title"
+                                name="title"
+                                autoComplete="title"
+                                autoFocus
+                                InputLabelProps={{ shrink: true }}
+                                value={eventTitle}
+                                onChange={(event) => setEventTitle(event.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <DateTimePicker
+                                sx={{ width: '100%' }}
+                                label="Start Date"
+                                id="start_date"
+                                name="start"
+                                value={eventStart}
+                                onChange={(value) => setEventStart(value)}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <DateTimePicker
+                                sx={{ width: '100%' }}
+                                label="End Date"
+                                id="end_date"
+                                name="end"
+                                value={eventEnd}
+                                onChange={(value) => setEventEnd(value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                margin="none"
+                                label="Description"
+                                id="description"
+                                name="description"
+                                autoComplete="description"
+                                autoFocus
+                                InputLabelProps={{ shrink: true }}
+                                value={eventDesc}
+                                onChange={(event) => setEventDesc(event.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                margin="none"
+                                label="Link"
+                                id="link"
+                                name="link"
+                                autoComplete="link"
+                                autoFocus
+                                InputLabelProps={{ shrink: true }}
+                                value={eventLink}
+                                onChange={(event) => setEventLink(event.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => onModalToggleClick(false)}>Cancel</Button>
+                    <Button type="submit">Confirm</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
