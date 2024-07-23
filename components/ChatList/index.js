18274ecb610f81e8f3d0f5e59dcded21fc849bad
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-07-23 16:18:43 +0800

    added option > actions in ChatList

--- components/ChatList/index.js
+++ components/ChatList/index.js
@@ -3,10 +3,17 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "@mui/material/styles";
 
+import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import Fab from "@mui/material/Fab";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Popover from '@mui/material/Popover';
 
 import EditNoteIcon from "@mui/icons-material/EditNote";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import CancelIcon from '@mui/icons-material/Cancel';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -19,6 +26,7 @@ export default function ChatList(props) {
 
   const [pasChatList, setPasChatList] = useState([]);
   const [fabAvatarIdx, setFabAvatarIdx] = useState(-1);
+  const [popoverAnchor, setPopoverAnchor] = useState(null);
 
   useEffect(() => {
   }, [])
@@ -28,15 +36,19 @@ export default function ChatList(props) {
   }, [props.passiveChatList])
 
   const onChatClick = (event, value) => {
-    if(props.onListChatClick) {
+    if (props.onListChatClick) {
       props.onListChatClick(value);
     }
   }
 
   const onRemoveClick = (event, value) => {
-    if(props.onChatListRemoveClick) {
+    if (props.onChatListRemoveClick) {
       props.onChatListRemoveClick(value);
     }
+  }
+
+  const onOptionClick = (event) => {
+    setPopoverAnchor(event.target);
   }
 
   return (
@@ -46,11 +58,11 @@ export default function ChatList(props) {
       </Fab> */}
 
       {pasChatList.map((item, idx) => (
-        <Fab 
-          key={idx} 
-          color="light" 
-          sx={{...CHAT_LIST.chatListFabAvatar, backgroundColor: theme.palette.light.dark}} 
-          onMouseEnter={() => setFabAvatarIdx(idx)} 
+        <Fab
+          key={idx}
+          color="light"
+          sx={{ ...CHAT_LIST.chatListFabAvatar, backgroundColor: theme.palette.light.dark }}
+          onMouseEnter={() => setFabAvatarIdx(idx)}
           onMouseLeave={() => setFabAvatarIdx(-1)}
         >
           <StyledBadge
@@ -64,12 +76,59 @@ export default function ChatList(props) {
           </StyledBadge>
 
           {fabAvatarIdx == idx ? (
-            <span style={{...CHAT_LIST.chatListFabClose, backgroundColor: theme.palette.secondary.main}} aria-label="fab-avatar-close" onClick={(event) => onRemoveClick(event, item)}>
+            <span style={{ ...CHAT_LIST.chatListFabClose, backgroundColor: theme.palette.secondary.main }} aria-label="fab-avatar-close" onClick={(event) => onRemoveClick(event, item)}>
               <FontAwesomeIcon icon={faTimes} size="md" />
             </span>
           ) : null}
         </Fab>
       ))}
+
+      {pasChatList.length >= 2 ? (
+        <IconButton color="dark" aria-label="chat-icon-close" onClick={onOptionClick}>
+          <MoreHorizIcon />
+        </IconButton>
+      ) : null}
+
+      <Popover
+        id={popoverAnchor ? 'simple-popover' : undefined}
+        open={popoverAnchor ? true : false}
+        anchorEl={popoverAnchor}
+        onClose={() => setPopoverAnchor(null)}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        disablePortal
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Button
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'flex-start',
+              color: theme.palette.dark.main
+            }}
+            startIcon={<CancelIcon />}
+          >
+            Close all chats
+          </Button>
+          <Button
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'flex-start',
+              color: theme.palette.dark.main
+            }}
+            startIcon={<RemoveIcon />}
+          >
+            Minimize all chats
+          </Button>
+        </Box>
+      </Popover>
     </div>
   );
 }
