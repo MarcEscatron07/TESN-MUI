ae436fdfcc75647d51aadc33dfa43e4673d3cef8
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-07-28 09:56:18 +0800

    optimized Chats > friends & groups

--- components/socket-client.js
+++ components/socket-client.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 import { SOCKET_PORT, SOCKET_GLOBAL_URL, SOCKET_LOCAL_URL } from '@/lib/variables';
 
-const SOCKET_URL = `${SOCKET_GLOBAL_URL}:${SOCKET_PORT}` ?? `${SOCKET_LOCAL_URL}:${SOCKET_PORT}`;
+const SOCKET_URL = SOCKET_GLOBAL_URL ? `${SOCKET_GLOBAL_URL}:${SOCKET_PORT}` : `${SOCKET_LOCAL_URL}:${SOCKET_PORT}`;
 
 export const socket = io(SOCKET_URL);
