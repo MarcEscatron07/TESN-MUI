5c7651ec79b5c55411d0d16757fbc8f45218d363
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-07-28 21:19:34 +0800

    updated Socket.io > cors > methods

--- socket-server.js
+++ socket-server.js
@@ -24,7 +24,7 @@ app.prepare().then(() => {
   const io = socketIo(server, {
     cors: {
       origin: `http://${HOST}:${PORT}`,
-      methods: ["GET", "POST"]
+      methods: ["GET", "PUT", "POST", "DELETE"]
     }
   });
 
