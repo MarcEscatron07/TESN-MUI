2a5d16f970933530d680ee83d819dcd64e8cd5b9
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-09-18 10:00:54 +0800

    added 404 page

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <div className="not-found-text">
+        404 | This page could not be found.
+      </div>
+    </div>
+  )
+}
