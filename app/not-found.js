0e973b29f8740c3b9f066c6af044c09df774d5f2
Marc Benedict Escatron <marc.escatron07@gmail.com>
2024-09-18 10:12:46 +0800

    updated not-found CSS

--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="not-found">
       <div className="not-found-text">
-        404 | This page could not be found.
+        <b>404</b> <span>This page could not be found.</span>
       </div>
     </div>
   )
